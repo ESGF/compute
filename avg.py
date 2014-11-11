53e860b5d6361fe975ff31e275e9b8e1d676a966
Sasha Ames <ames4@aims2.llnl.gov>
2014-11-11 15:59:00 -0800

    simple averager script

---
+++ avg.py
@@ -0,0 +1,16 @@
+import cdms2,sys,os,cdutil,MV2
+f=cdms2.open(sys.argv[1])
+V=f["clt"]
+
+OUT = MV2.zeros((V.shape[0],))
+
+for i in range(V.shape[0]):
+
+  s=V[i]
+  OUT[i]=cdutil.averager(s,axis="xy")
+
+OUT.setAxis(0,V.getTime().clone())
+OUT.id=V.id
+
+print OUT
+
