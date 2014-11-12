6ffa7052ec69cccb8e679ec5f2c520e0903ead1a
Sasha Ames <ames4@aims1.llnl.gov>
2014-11-12 12:54:02 -0800

    fix add head

--- add_head.py
+++ add_head.py
@@ -2,8 +2,10 @@ import sys
 
 key = sys.argv[1]
 
+count = 0
+
 for line in sys.stdin:
 
-    sys.stdout.write(key + " " + line)
-
+    sys.stdout.write(key + " " + count +  " " + line)
+    count = count + 1
 
